622729b281ca2cb8acd6d43c2ff7da59bda6d450
polsito <lordgelsin26@gmail.com>
2025-01-12 08:49:59 -0500

    fix: adding credentials in provider email

--- src/modules/mail/adapters/out/mail-provider.service.ts
+++ src/modules/mail/adapters/out/mail-provider.service.ts
@@ -1,6 +1,12 @@
 import { SES, SendRawEmailCommand } from '@aws-sdk/client-ses';
 
 export class EmailProviderService {
-  public static sesClient = new SES({ region: process.env.REGION });
+  public static sesClient = new SES({
+    region: process.env.REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
   public static sendCommand = SendRawEmailCommand;
 }
