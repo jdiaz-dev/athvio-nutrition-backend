f2e66a4cc2c332be9798f13dc6d3fd0f9f211c30
polsito <lordgelsin26@gmail.com>
2025-01-14 19:48:04 -0500

    fix: debuggin credentials

--- src/modules/mail/adapters/out/mail-provider.service.ts
+++ src/modules/mail/adapters/out/mail-provider.service.ts
@@ -1,5 +1,7 @@
 import { SES, SendRawEmailCommand } from '@aws-sdk/client-ses';
 
+console.log('---------process.env.AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID)
+console.log('---------process.env.AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY)
 export class EmailProviderService {
   public static sesClient = new SES({
     region: process.env.REGION,
