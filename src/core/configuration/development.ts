d406d8988a28f11223b961508d6c2073fa27c19b
polsito <lordgelsin26@gmail.com>
2025-02-14 13:25:44 -0500

    fix: enabling apollo graphq studio in development

--- src/core/configuration/development.ts
+++ src/core/configuration/development.ts
@@ -7,7 +7,7 @@ export const development: IEnvironment = {
   },
   security: { rateLimit: { ttl: process.env.RATE_LIMIT_TLL, limit: process.env.RATE_LIMIT_VALUE } },
   tokenKey: process.env.SIGN_TOKEN,
-  whiteListOrigins: [process.env.ORIGIN_WEB_LOCAL, process.env.ORIGIN_MOBILE_LOCAL],
+  whiteListOrigins: [process.env.ORIGIN_WEB_LOCAL, process.env.ORIGIN_MOBILE_LOCAL, process.env.ORIGIN_APOLLO_GRAPHQL_STUDIO],
   foodProvider: {
     foodApi: {
       edamamFoodParserUrl: process.env.EDAMAM_FOOD_PARSER_URL,
