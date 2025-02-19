19ea7f9a9e9b89f5c99d6a16d51cc7f444fcd6ce
polsito <lordgelsin26@gmail.com>
2025-02-19 18:12:01 -0500

    saving comments

--- database/diseases.js
+++ database/diseases.js
@@ -1,7 +1,11 @@
 db.Diseases.deleteMany({});
 
 db.Diseases.insertMany([
+  { name: 'alzheimer', createdAt: new Date(), updatedAt: new Date() },
+  { name: 'autism', createdAt: new Date(), updatedAt: new Date() },
+  { name: 'cancer', createdAt: new Date(), updatedAt: new Date() },
   { name: 'candidiasis', createdAt: new Date(), updatedAt: new Date() },
+  { name: 'constipation', createdAt: new Date(), updatedAt: new Date() },
   {
     name: 'hypothyrodism',
     symptoms: [],
@@ -10,4 +14,30 @@ db.Diseases.insertMany([
     updatedAt: new Date(),
   },
   { name: 'leaky gut', createdAt: new Date(), updatedAt: new Date() },
+  { name: 'parkinson', createdAt: new Date(), updatedAt: new Date() },
+  { name: 'psoriasis', createdAt: new Date(), updatedAt: new Date() },
 ]);
+//  diseases | preferences and restrictions
+
+/* 
+  - to have data:
+    - goals 
+    - diseases, or historial of diseases
+    - lab test, blood test (results , documents)
+    - nutritic approach (vegetarian, carnivore, keto, etc)
+    - caloric intake
+*/
+
+/* 
+* heal diseases
+  parasites
+    - coconut oil, castor oil, 
+    - keto
+
+  candida
+    - keto 
+
+  constipation
+    - keto 
+
+*/
