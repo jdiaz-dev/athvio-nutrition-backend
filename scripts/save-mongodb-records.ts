3965e68c5160c8a3573e2ed34b27c1a27de6e871
polsito <lordgelsin26@gmail.com>
2025-04-03 18:37:32 -0500

    fix: script

--- scripts/save-mongodb-records.ts
+++ scripts/save-mongodb-records.ts
@@ -1,24 +1,35 @@
 import fs from 'fs';
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand, PutObjectCommandOutput, S3Client } from '@aws-sdk/client-s3';
 
-const bucketName = 'nutritional-meals-bucket';
-const fileName = 'mongo_data.json';
-
+const bucketName = 'mongodb-records';
+const filePaths = [
+  {
+    path: '/home/polsito/Mongodb/athvio.NutritionalMeals.json',
+    fileName: 'athvio.NutritionalMeals.json',
+  },
+  {
+    path: '/home/polsito/Mongodb/athvio.Programs.json',
+    fileName: 'athvio.Programs.json',
+  },
+];
 async function saveRecords() {
+  const promises: Promise<PutObjectCommandOutput>[] = [];
   try {
-    const filePath = '/home/polsito/Mongodb/athvio.NutritionalMeals.json';
-    const fileContent = fs.readFileSync(filePath);
+    for (let x = 0; x < filePaths.length; x++) {
+      const fileContent = fs.readFileSync(filePaths[x].path);
 
-    const command = new PutObjectCommand({
-      Bucket: bucketName,
-      Key: fileName,
-      Body: fileContent,
-    });
+      const command = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: filePaths[x].fileName,
+        Body: fileContent,
+      });
 
-    const s3client = new S3Client({
-      region: process.env.REGION,
-    });
-    await s3client.send(command);
+      const s3client = new S3Client({
+        region: process.env.REGION,
+      });
+      promises.push(s3client.send(command));
+    }
+    await Promise.all(promises);
   } catch (error) {
     console.error('Error saving records to S3', error);
   }
