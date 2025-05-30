2e49bcb4d70ac4d8ac46e7ed1fc75b59d31dc29b
polsito <lordgelsin26@gmail.com>
2025-05-29 21:45:29 -0500

    fix

--- src/modules/professionals/professionals/adapters/out/professional.schema.ts
+++ src/modules/professionals/professionals/adapters/out/professional.schema.ts
@@ -37,7 +37,7 @@ export const OrganizationSchema = SchemaFactory.createForClass(Organization);
 @Schema({ timestamps: true, collection: 'Professionals' })
 export class Professional extends BaseSchema {
   @Field(() => ID)
-  _id!: string;
+  _id!: MongooseSchema.Types.ObjectId;
 
   @Field(() => User || String)
   @Prop({ type: MongooseSchema.Types.ObjectId, required: false, ref: User.name })
