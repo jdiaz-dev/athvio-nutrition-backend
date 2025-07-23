e497f4b2ecc4725ca7ad1df2ca5513390e749bb4
polsito <lordgelsin26@gmail.com>
2025-07-22 21:56:15 -0500

    fix

--- src/modules/program-generator/food-analyzer/adapters/in/food-analyzers.resolver.ts
+++ src/modules/program-generator/food-analyzer/adapters/in/food-analyzers.resolver.ts
@@ -1,10 +1,9 @@
-import { Args, Info, Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 
 import { UseGuards } from '@nestjs/common';
 import { AuthorizationGuard } from 'src/modules/auth/auth/adapters/in/web/guards/authorization.guard';
 import { AuthorizationProfessionalGuard } from 'src/shared/guards/authorization-professional.guard';
 import { FoodAnalyzer } from 'src/modules/program-generator/food-analyzer/adapters/out/food-analyzer.schema';
-import { selectorExtractorForAggregation } from 'src/shared/helpers/graphql-helpers';
 import { GetAnalyzedFoodsDto } from 'src/modules/program-generator/food-analyzer/adapters/in/dtos/get-analyzed-foods.dto';
 import { FoodAnalyzersManagerService } from 'src/modules/program-generator/food-analyzer/application/food-analyzers-manager.service';
 
@@ -14,12 +13,8 @@ export class FoodAnalyzersResolver {
   constructor(private readonly faps: FoodAnalyzersManagerService) {}
 
   @Query(() => [FoodAnalyzer])
-  async getAnalyzedFoods(
-    @Args('input') dto: GetAnalyzedFoodsDto,
-    @Info(...selectorExtractorForAggregation()) selectors: Record<string, number>,
-  ): Promise<FoodAnalyzer[]> {
-    selectors;
-    const diseases = await this.faps.getFoodAnalyzers(dto.internalFoods);
-    return diseases;
+  async getAnalyzedFoods(@Args('input') dto: GetAnalyzedFoodsDto): Promise<FoodAnalyzer[]> {
+    const analyzedFoods = await this.faps.getFoodAnalyzers(dto.internalFoods);
+    return analyzedFoods;
   }
 }
