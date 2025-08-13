baf0a7a879b00df8cfe5646de73ecc98d65d5d35
polsito <lordgelsin26@gmail.com>
2025-08-12 19:01:28 -0500

    fix

--- script.sh
+++ script.sh
@@ -4,6 +4,4 @@ npx swc src -d dist --config-file .swcrc
 npx swc configuration -d dist --config-file .swcrc
 dotenvx run -f .env.development -- npx nest start --watch -b swc --type-check
 
-
-
 # dotenvx run -f .env.development -- node dist/src/main.js
