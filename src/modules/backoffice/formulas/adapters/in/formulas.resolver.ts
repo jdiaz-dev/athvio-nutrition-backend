f9edb920f6ff832dcfed3052191b43ad64b0aa5e
polsito <lordgelsin26@gmail.com>
2025-09-04 10:44:57 -0500

    fix

--- src/modules/backoffice/formulas/adapters/in/formulas.resolver.ts
+++ src/modules/backoffice/formulas/adapters/in/formulas.resolver.ts
@@ -1,13 +1,9 @@
-import { UseGuards } from '@nestjs/common';
 import { Info, Query, Resolver } from '@nestjs/graphql';
-import { AuthorizationGuard } from 'src/modules/auth/auth/adapters/in/web/guards/authorization.guard';
-import { AuthorizationProfessionalGuard } from 'src/shared/guards/authorization-professional.guard';
 import { FormulasManagerService } from 'src/modules/backoffice/formulas/application/formulas-manager.service';
 import { Formula } from 'src/modules/backoffice/formulas/adapters/out/formula.schema';
 import { selectorExtractorForAggregation } from 'src/shared/helpers/graphql-helpers';
 
 @Resolver()
-@UseGuards(...[AuthorizationGuard, AuthorizationProfessionalGuard])
 export class FormulasResolver {
   constructor(private readonly fms: FormulasManagerService) {}
 
--- src/modules/backoffice/formulas/adapters/out/formula.schema.ts
+++ src/modules/backoffice/formulas/adapters/out/formula.schema.ts
@@ -1,26 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { BaseSchema } from 'src/shared/schemas/base.schema';
 
+@ObjectType()
 @Schema({ _id: false, timestamps: false })
 export class Constant {
+  @Field()
   @Prop({ type: String, required: true })
   spanishConstantName: string;
 
+  @Field()
   @Prop({ type: Number, required: true })
   value: number;
 
+  @Field({ nullable: true })
   @Prop({ type: String, required: false })
   case?: string;
 }
 export const NutrientsSchema = SchemaFactory.createForClass(Constant);
 
+@ObjectType()
 @Schema({ _id: false, timestamps: false })
 export class FormulaGroup {
+  @Field()
   @Prop({ type: String, required: false })
   spanishFormulaName: string;
 
+  @Field(() => [Constant])
   @Prop({ type: [NutrientsSchema], required: true })
   constants: Constant[];
 }
@@ -29,9 +36,11 @@ export const FormulaGroupSchema = SchemaFactory.createForClass(FormulaGroup);
 @ObjectType()
 @Schema({ timestamps: true, collection: 'Formulas' })
 export class Formula extends BaseSchema {
+  @Field()
   @Prop({ type: String, required: true })
   spanishGroupName: string;
 
+  @Field(() => [FormulaGroup])
   @Prop({ type: [FormulaGroupSchema], required: false })
   formulaGroups: FormulaGroup[];
 }
