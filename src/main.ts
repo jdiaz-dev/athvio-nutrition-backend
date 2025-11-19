0da808ca3d8fbcbbf2b5573dc36e521e89425b0f
jonathanDiaz <lordgelsin26@gmail.com>
2025-11-19 17:43:40 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap(): Promise<void> {
   const configService = app.get(ConfigService);
   const whiteListOrigins = configService.get<string[]>('whiteListOrigins');
   const port = configService.get<string>('port') || process.env.PORT;
-
+  console.log('------whiteListOrigins', whiteListOrigins)
   app.enableCors({
     origin: whiteListOrigins,
     credentials: true,
