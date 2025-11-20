90a37b7b7e57db60dbc7f068ab9db443fd3be797
jonathanDiaz <lordgelsin26@gmail.com>
2025-11-20 11:14:59 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -41,11 +41,9 @@ async function bootstrap(): Promise<void> {
     origin: whiteListOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: "*"//['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  // This still works via middie, though in the long term you'd probably switch
-  // to @fastify/helmet for native Fastify integration.
   app.use(
     helmet({
       crossOriginEmbedderPolicy: false,
