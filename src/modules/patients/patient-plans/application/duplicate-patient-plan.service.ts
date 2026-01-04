494b10a0403a21772a42812d2be11f1a018e460d
jonathanDiaz <lordgelsin26@gmail.com>
2026-01-03 21:39:02 -0500

    fix

--- src/modules/patients/patient-plans/application/duplicate-patient-plan.service.ts
+++ src/modules/patients/patient-plans/application/duplicate-patient-plan.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
 import { DuplicatePatientPlanDto } from 'src/modules/patients/patient-plans/adapters/in/web/dtos/plan/duplicate-patient-plan.dto';
 import { PatientPlan } from 'src/modules/patients/patient-plans/adapters/out/patient-plan.schema';
 import { PatientPlansPersistenceService } from 'src/modules/patients/patient-plans/adapters/out/patient-plans-persistence.service';
@@ -14,16 +15,18 @@ export class DuplicatePatientPlanService {
     const _patientPlan = await this.cpps.getPatientPlan({ professional, patient, patientPlan }, selectors);
 
     delete _patientPlan._id;
+    delete _patientPlan.uuid;
     delete _patientPlan.assignedDate;
     for (let x = 0; x < _patientPlan.meals.length; x++) {
       delete _patientPlan.meals[x]._id;
       delete _patientPlan.meals[x].updatedAt;
     }
-    _patientPlan;
+
     const programUpdated = await this.cpps.createPatientPlan({
       ..._patientPlan,
       patient,
       assignedDate: rest.assignedDate,
+      uuid: randomUUID(),
     });
     return programUpdated;
   }
