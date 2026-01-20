49b376b196a57cd76dac9fd9615b919f94d02f65
jonathanDiaz <lordgelsin26@gmail.com>
2026-01-20 16:14:22 -0500

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   constructor() {}
 
-  @Get("ping")
+  @Get("health")
   getHello(): string {
     return 'Athvio server!';
   }
