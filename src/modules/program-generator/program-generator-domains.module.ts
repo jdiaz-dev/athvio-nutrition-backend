1dd5fd243a4b75b9845bf20305c8f096d516feed
Jonathan Díaz <jonathan.diaz@externo.auna.org>
2026-05-24 23:18:56 -0500

    fix

--- src/modules/program-generator/program-generator-domains.module.ts
+++ src/modules/program-generator/program-generator-domains.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DiseaseCausesModule } from 'src/modules/program-generator/disease-causes/disease-causes.module';
 import { DiseasesModule } from 'src/modules/program-generator/diseases/diseases.module';
 import { FoodAnalyzersModule } from 'src/modules/program-generator/food-analyzer/food-analyzer.module';
 import { AIproviderModule } from 'src/modules/program-generator/artificial-intelligence/ai-provider.module';
-// import { Neo4jModule } from 'src/modules/program-generator/neo4j/neo4j.module';
+import { Neo4jModule } from 'src/modules/program-generator/neo4j/neo4j.module';
 import { NutritionalPreferencesModule } from 'src/modules/program-generator/nutritional-preferences/nutritional-preferences.module';
 import { ProgramGeneratorModule } from 'src/modules/program-generator/program-generator/program-generator.module';
-// import { Neo4jConfig } from 'src/modules/program-generator/shared/types';
+import { Neo4jConfig } from 'src/modules/program-generator/shared/types';
 
 @Module({
   imports: [
-    /* Neo4jModule.forRootAsync({
+    Neo4jModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService): Neo4jConfig => ({
@@ -22,7 +22,7 @@ import { ProgramGeneratorModule } from 'src/modules/program-generator/program-ge
         username: configService.get<string>('database.neo4j.username'),
         password: configService.get<string>('database.neo4j.password'),
       }),
-    }), */
+    }),
     DiseaseCausesModule,
     DiseasesModule,
     NutritionalPreferencesModule,
