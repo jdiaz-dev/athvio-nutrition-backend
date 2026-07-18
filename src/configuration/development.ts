10f2d0f0f5e4accd2cf9ada7dcfa04a708b30e51
Jonathan Díaz <jonathan.a.diaz.m@gmail.com>
2026-07-17 21:18:07 -0500

    fix

--- src/configuration/development.ts
+++ src/configuration/development.ts
@@ -15,7 +15,6 @@ export const development: IEnvironment = {
     },
   },
   storage: {
-    // foodImagesStorage: process.env.FOOD_IMAGES_STORAGE,
     foodImagesStorageUrl: process.env.FOOD_IMAGES_STORAGE_URL,
     internalFoodImagesStorage: process.env.INTERNAL_FOOD_IMAGES_STORAGE,
     internalFoodImagesDirectory: process.env.INTERNAL_FOOD_IMAGES_DIRECTORY,
